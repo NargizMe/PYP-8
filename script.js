c352c39b99ede2b03a9441bcb26a3363cd79d55a
Your Name <you@example>
2022-09-01 13:58:26 +0400

    working started

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
 let valueOfBox = document.querySelectorAll('.value');
-let canditdateOfBox = document.querySelectorAll('.candidates');
+let canditdatesOfBox = document.querySelectorAll('.candidates');
+let tdsBox = document.querySelectorAll('td');
+let candidateSwitch = document.querySelector('#candidate-switch');
+let sudokuNumbers = document.querySelectorAll('.number-control');
+
+let previousTd;
+tdsBox.forEach((td) => {
+    td.addEventListener('click', () =>{
+        if(td.childElementCount === 2){
+            previousTd?.classList.remove('selected');
+            td.setAttribute('class', 'selected');
+            previousTd = td;
+        }
+    })
+})
+
+sudokuNumbers.forEach((btn) => {
+    btn.addEventListener('click', function() {
+        if(candidateSwitch.checked){
+            console.log(this.textContent);
+        }
+    })
+})
+
+candidateSwitch.addEventListener('click', function() {
+    return this.checked;
+})
+
+
 
 
